8306a24fb98b956077fc40602d86096de76da06a
DauweQuinten <quinten.dauwe@student.howest.be>
2023-05-24 16:32:36 +0200

    Yeet weggeyeet

--- Assets/Tutorial/Scripts/FlowManager.cs
+++ Assets/Tutorial/Scripts/FlowManager.cs
@@ -7,18 +7,31 @@ public class FlowManager : MonoBehaviour
 {
     [SerializeField] CheckTrigger trigger;
     [SerializeField] NormalTextPanel textPanel;
+    [SerializeField] GameObject tool01;
+
+    private Vector3 tool01StartPos;
+
+    private void Awake()
+    {
+        tool01StartPos = tool01.transform.position; 
+        tool01StartPos.y += 0.1f;
+    }
+
+
     private void Update()
     {
         Debug.Log(textPanel.CurrentPageIndex);
     }
 
-
     public void FinishTask01()
     {
         if(textPanel.CurrentPageIndex == 2)
         {
-            Debug.Log("Task finished!");
             textPanel.NextPage();
+        }
+        else if(textPanel.CurrentPageIndex < 2)
+        {
+            tool01.transform.position = tool01StartPos;
         }
     }
 }
