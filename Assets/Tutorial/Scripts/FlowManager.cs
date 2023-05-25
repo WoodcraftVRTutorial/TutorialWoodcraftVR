092d09e0a081fb7829168134ab57887dbb4e83fa
DauweQuinten <quinten.dauwe@student.howest.be>
2023-05-25 11:04:30 +0200

    Added particle magic 🪄

--- Assets/Tutorial/Scripts/FlowManager.cs
+++ Assets/Tutorial/Scripts/FlowManager.cs
@@ -1,37 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
+using static UnityEngine.ParticleSystem;
 
 public class FlowManager : MonoBehaviour
 {
     [SerializeField] CheckTrigger trigger;
     [SerializeField] NormalTextPanel textPanel;
     [SerializeField] GameObject tool01;
+    [SerializeField] GameObject particlePrefab;
+    [SerializeField] GameObject particleSpawnPoint;
+
+
 
     private Vector3 tool01StartPos;
 
     private void Awake()
     {
-        tool01StartPos = tool01.transform.position; 
+        tool01StartPos = tool01.transform.position;
         tool01StartPos.y += 0.1f;
-    }
-
-
-    private void Update()
-    {
-        Debug.Log(textPanel.CurrentPageIndex);
     }
 
     public void FinishTask01()
     {
-        if(textPanel.CurrentPageIndex == 2)
+        if (textPanel.CurrentPageIndex == 2)
         {
             textPanel.NextPage();
+            SpawnParticles();
         }
-        else if(textPanel.CurrentPageIndex < 2)
+        else if (textPanel.CurrentPageIndex < 2)
         {
             tool01.transform.position = tool01StartPos;
         }
+    }
+
+    private void SpawnParticles()
+    {
+        Instantiate(particlePrefab, particleSpawnPoint.transform);
     }
 }
